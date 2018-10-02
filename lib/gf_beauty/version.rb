0aac931ed9ff97bf2160adea46307fb03d9aa52f
Dylan Hoang <dylan.hoang@somotsoftvn.com>
2018-10-02 10:33:09 +0700

    init

---
+++ lib/gf_beauty/version.rb
@@ -0,0 +1,3 @@
+module GfBeauty
+  VERSION = "0.1.0"
+end
---
+++ spec/gf_beauty_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GfBeauty do
+  it "has a version number" do
+    expect(GfBeauty::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
